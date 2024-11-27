e86bc572113d5832cfe8b650d37f82fe6cb6aa7e
Andrew McCallum <66245546+stretch0@users.noreply.github.com>
2024-11-27 13:50:41 +0000

    Updated loading state (#7)

--- app/[secretId]/page.tsx
+++ app/[secretId]/page.tsx
@@ -11,6 +11,9 @@ export default async function GetSecret({ params, searchParams }: GetSecretProps
       <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center align-items-center">
           <GetSecretForm secretId={secretId} privateKey={privateKey} />
+          <p className="text-sm text-gray-500 italic -mt-5">
+            This secret will be destroyed after it is viewed.
+          </p>
         </main>
       </div>
     </>
