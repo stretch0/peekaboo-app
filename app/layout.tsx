ea66fe0b0bd560e257499a74569104b3724862a0
stretch0 <amccallum89@gmail.com>
2024-12-19 14:05:56 +0000

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -81,6 +82,7 @@ export default function RootLayout({
         </nav>
 
         {children}
+        <Analytics />
       </body>
 
     </html>
